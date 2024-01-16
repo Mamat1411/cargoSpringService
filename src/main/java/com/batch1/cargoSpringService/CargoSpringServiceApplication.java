4d4de54fdfccaf3ffc6b213f9253e75e3162efa0
Mamat1411 <muhammadmujahid14@gmail.com>
2024-01-16 21:23:57 +0700

    Initial Commit

---
+++ src/main/java/com/batch1/cargoSpringService/CargoSpringServiceApplication.java
@@ -0,0 +1,13 @@
+package com.batch1.cargoSpringService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CargoSpringServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CargoSpringServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/batch1/cargoSpringService/CargoSpringServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.batch1.cargoSpringService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CargoSpringServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
