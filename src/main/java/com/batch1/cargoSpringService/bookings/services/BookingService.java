6059a8cffb02b36784a20d0689c700fa80c86d7b
Mamat1411 <muhammadmujahid14@gmail.com>
2024-01-19 10:32:17 +0700

    Kafka Consumer Setup Done

--- src/main/java/com/batch1/cargoSpringService/bookings/services/BookingService.java
+++ src/main/java/com/batch1/cargoSpringService/bookings/services/BookingService.java
@@ -3,10 +3,13 @@ package com.batch1.cargoSpringService.bookings.services;
 import java.util.List;
 
 import com.batch1.cargoSpringService.bookings.entities.Booking;
+import com.fasterxml.jackson.core.JsonProcessingException;
 
 public interface BookingService {
     List<Booking> getAllBookings();
     Booking getBookingById(Long id);
     Booking saveBooking(Booking booking);
     void deleteBooking(Long id);
+
+    void receiveBooking(String data) throws JsonProcessingException;
 }
--- src/main/java/com/batch1/cargoSpringService/bookings/services/impl/BookingServiceImpl.java
+++ src/main/java/com/batch1/cargoSpringService/bookings/services/impl/BookingServiceImpl.java
@@ -3,15 +3,20 @@ package com.batch1.cargoSpringService.bookings.services.impl;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
 
 import com.batch1.cargoSpringService.bookings.entities.Booking;
 import com.batch1.cargoSpringService.bookings.repositories.BookingRepository;
 import com.batch1.cargoSpringService.bookings.services.BookingService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class BookingServiceImpl implements BookingService{
 
     @Autowired
     private BookingRepository bookingRepository;
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
 
     @Override
     public List<Booking> getAllBookings() {
@@ -31,6 +36,13 @@ public class BookingServiceImpl implements BookingService{
     @Override
     public void deleteBooking(Long id) {
         bookingRepository.deleteById(id);
+    }
+
+    @Override
+    @KafkaListener(topics = "booking", groupId = "booking_groupid")
+    public void receiveBooking(String data) throws JsonProcessingException{
+        Booking booking = objectMapper.readValue(data, Booking.class);
+        bookingRepository.save(booking);
     }
     
 }
